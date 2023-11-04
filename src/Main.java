77ff9dd270c1693ebc477afeb6088b4179d526bd
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-11-04 13:40:47 -0700

    Coded the game loop
    Fixing bound errors and minor issues

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,78 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
+    static int origin = 0;
+    static int bound = 100;
+    static int attempts = 10;
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Welcome to guessing game");
+        System.out.println("Guess the Number Game!");
+        welcomeMessage();
+
     }
+
+    public static void startNewGame() {
+        gameLoop(1);
+    }
+
+    public static void welcomeMessage() {
+        System.out.println("\n");
+        System.out.println("1. Start a new game.");
+        System.out.println("2. Change origin (0).");
+        System.out.println("3. Change bound (100).");
+        System.out.println("4. Change attempts amount (10).");
+        System.out.println("5. Quit.");
+        System.out.println("\n");
+
+        int response = Integer.parseInt(scanner.nextLine());
+        gameLoop(response);
+    }
+    public static void gameLoop(int response) {
+        // this is where the actual game play will happen!
+        switch (response) {
+            case 1:
+                System.out.printf("Guess the number between %d and %d.\n", origin, bound);
+                System.out.printf("%d attempts left.\n", attempts);
+
+                for (int i = attempts; i > 0; i--) {
+                    int currNumber = Integer.parseInt(scanner.nextLine());
+                    int randomInt = origin + (int) (Math.random() * (bound - origin));
+                    if (currNumber == randomInt) {
+                        System.out.printf("You won! computer selected: %d and u selected: %d\n", randomInt, currNumber);
+                        welcomeMessage();
+                        break;
+                    } else if (currNumber < randomInt) {
+                        --attempts;
+                        System.out.printf("The number (%d) is greater than yours!\n", randomInt);
+                        System.out.printf("%d attempts left.\n", i);
+                    } else {
+                        System.out.printf("The number (%d) is less than yours!\n", randomInt);
+                        System.out.printf("%d attempts left.\n", i);
+                    }
+                }
+                break;
+            case 2:
+                // change the origin and start the game from beginning!
+                System.out.println("Enter new origin you would like: ");
+                origin = Integer.parseInt(scanner.nextLine());
+                startNewGame();
+                break;
+            case 3:
+                System.out.println("Enter the new bound: ");
+                bound = Integer.parseInt(scanner.nextLine());
+                startNewGame();
+                break;
+            case 4:
+                System.out.println("Enter the new amount of attempts: ");
+                attempts = Integer.parseInt(scanner.nextLine());
+                startNewGame();
+                break;
+            case 5:
+                System.out.println("Thank you for playing!");
+                gameLoop(5);
+                break;
+            default:
+                // nothing for now!
+        }
+    }
+
 }
